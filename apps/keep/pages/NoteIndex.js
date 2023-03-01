b1b0864413625871422dc8f4016bb7d08854303b
yiftach <yfty777@gmail.com>
2023-03-01 11:58:13 +0200

    yiftach commit

--- apps/keep/pages/NoteIndex.js
+++ apps/keep/pages/NoteIndex.js
@@ -1,1 +1,41 @@
-export default {}
+import { noteService } from "../services/note.service.js"
+
+import noteList from '../cmps/NoteList'
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
+
+
+export default {
+    template: `
+        <section class="note-index">
+        <textarea v-model="message" placeholder="add your note.."></textarea>
+            <noteList 
+                :notes="filteredNotes" 
+                @remove="removeNote"/>
+          
+        </section>
+    `,
+    data() {
+        return {
+            notes: [],
+        }
+    },
+    created() {
+        noteService.query()
+            .then(notes => this.notes = notes)
+    },
+    methods: {
+        removeNote(noteId) {
+            noteService.remove(noteId)
+                .then(() => {
+                    const idx = this.notes.findIndex(note => note.id === noteId)
+                    this.notes.splice(idx, 1)
+                    showSuccessMsg('Car removed')
+                })
+                .catch(err => {
+                    showErrorMsg('Car remove failed')
+                })
+        },
+        onSaveNote(newNote){
+            this.notes.unshift(newNote)
+        }}
+    }
---
+++ apps/keep/services/note.service.js
@@ -0,0 +1,109 @@
+'use strict'
+
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const CAR_KEY = 'noteDB'
+
+const notes = [
+    {
+    id: 'n101',
+    createdAt: 1112222,
+    type: 'NoteTxt',
+    isPinned: true,
+    style: {
+    backgroundColor: '#00d'
+    },
+    info: {
+    txt: 'Fullstack Me Baby!'
+    }
+    },
+    {
+    id: 'n102',
+    type: 'NoteImg',
+    isPinned: false,
+    info: {
+    url: 'http://some-img/me',
+    title: 'Bobi and Me'
+    },
+    style: {
+    backgroundColor: '#00d'
+    }
+    },
+    {
+    id: 'n103',
+    type: 'NoteTodos',
+    isPinned: false,
+    info: {
+    title: 'Get my stuff together',
+    todos: [
+    { txt: 'Driving license', doneAt: null },
+    { txt: 'Coding power', doneAt: 187111111 }
+    ]
+    }
+    }
+    ]
+
+    _createNotes()
+
+export const noteservice = {
+  query,
+  get,
+  remove,
+  save,
+  getEmptyNote,
+  removeReview,
+  addReview,
+}
+
+function query(filterBy = {}) {
+  return storageService.query(note_KEY).then((notes) => {
+    if (filterBy.txt) {
+      const regex = new RegExp(filterBy.txt, 'i')
+      notes = notes.filter((note) => regex.test(note.title))
+    }
+    if (filterBy.maxPrice) {
+      notes = notes.filter((note) => note.listPrice.amount <= filterBy.maxPrice)
+    }
+    return notes
+  })
+}
+
+function get(noteId) {
+  return storageService.get(note_KEY, noteId)
+}
+
+function remove(noteId) {
+  return storageService.remove(note_KEY, noteId)
+}
+
+function save(note) {
+  if (note.id) {
+    return storageService.put(note_KEY, note)
+  } else {
+    return storageService.post(note_KEY, note)
+  }
+}
+
+// function getEmptynote(title = '', price = 0) {
+//   return {
+//     id: '',
+//     title,
+//     subtitle: utilService.makeLorem(8),
+//     authors: [utilService.makeLorem(2)],
+//     categories: [utilService.makeLorem(1), utilService.makeLorem(1)],
+
+//     publishedDate: 1920 + Math.random() * 100,
+//     description: utilService.makeLorem(20),
+
+//     pageCount: Math.random() * 1000,
+//     thumbnail: 'https://drmichellebraun.com/wp-content/uploads/2016/10/note-Placeholder.png',
+//     language: 'en',
+//     listPrice: {
+//       amount: price,
+//       currencyCode: 'ILS',
+//       isOnSale: true,
+//     },
+//   }
+// }
+    
