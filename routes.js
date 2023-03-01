66a7303e677387378ee7174eaf0ee4107e011732
Elad Millar <eladmillar@gmail.com>
2023-03-01 10:19:29 +0200

    first navigation

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 import HomePage from './views/HomePage.js'
 import AboutUs from './views/AboutUs.js'
+import MailIndex from './apps/mail/pages/MailIndex.js'
+import NoteIndex from './apps/keep/pages/NoteIndex.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
@@ -13,6 +15,14 @@ const routerOptions = {
 		{
 			path: '/about',
 			component: AboutUs,
+		},
+		{
+			path: '/mail',
+			component: MailIndex,
+		},
+		{
+			path: '/note',
+			component: NoteIndex,
 		},
 	],
 }
